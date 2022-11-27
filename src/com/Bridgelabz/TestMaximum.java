fea0c3ba391732262cb33dbf26f72ab378c337ed
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-27 20:23:10 +0530

    Added :UC2 Among 3 float numbers maximum is founded.

--- src/com/Bridgelabz/TestMaximum.java
+++ src/com/Bridgelabz/TestMaximum.java
@@ -29,14 +29,15 @@ public class TestMaximum<T extends Comparable<T>> {
 	
 		
 	public static <T> void printMax(T x, T y, T z, T max) {
-		System.out.printf("Max of integers %s,%s and %s is--> %s\n",x,y,z,max);
+		System.out.printf("Max of %s,%s and %s is--> %s\n",x,y,z,max);
 	}
 	
 	public static void main(String[] args) {
 		
 		Integer xInt=7, yInt=4, zInt=5;
-		
+		Float xFl=6.6f, yFl=8.8f, zFl=7.7f;
 		new TestMaximum(xInt, yInt, zInt).maximum();
+		new TestMaximum(xFl, yFl, zFl).maximum();
 		
 	}
 }
